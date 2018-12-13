dcce2067a92bf28e0c0d46c149409873bd244b38
TarlonGomes <tarlonppgg@gmnail.com>
2018-12-13 14:54:17 -0200

    testando

---
+++ rest-api/src/main/java/br/edu/utfpr/md/restapi/model/Pessoa.java
@@ -0,0 +1,86 @@
+package br.edu.utfpr.md.restapi.model;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Past;
+
+@Entity
+@Table(name = "tb_pessoa")
+public class Pessoa implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(nullable = false)
+    @NotNull
+    private String nome;
+
+    @Temporal(TemporalType.DATE)
+    //@Past
+    private Date dataNascimento;
+
+    private String email;
+
+    private String senha;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Date getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(Date dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    /*
+    public void setDataNascimentoByDate(Date dataNascimento) {
+        this.dataNascimento = new GregorianCalendar();
+        this.dataNascimento.setTime(dataNascimento);
+    }
+    */
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    @Override
+    public String toString() {
+        return "Pessoa{" + "id=" + id + ", nome=" + nome + ", dataNascimento=" + dataNascimento + ", email=" + email + ", senha=" + senha + '}';
+    }
+}
